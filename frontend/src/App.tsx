34b4b5540d81ad0e0b2291d5e08852f2ca251836
Mohamed Aslam <mohamedaslammn.2003@gmail.com>
2023-07-18 15:24:59 +0530

    Gig Card

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,7 +1,25 @@
 import "./App.css";
+import GigsCard from "./components/GigsCard";
 
 function App() {
-  return <h1>Hello</h1>;
+  const DUMMY = {
+    gigsTitle: "A React Page",
+    gigDescription:
+      "Provided with Design structure, Giggs requested to create the same with React",
+    gigPrice: "300rs",
+    postedBy: {
+      name: "Eric Vone",
+      avatar:
+        "https://images.unsplash.com/photo-1535713875002-d1d0cf377fde?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8M3x8YXZhdGFyfGVufDB8fDB8fHww&w=1000&q=80",
+    },
+    gigImage:
+      "https://repository-images.githubusercontent.com/37153337/9d0a6780-394a-11eb-9fd1-6296a684b124",
+  };
+  return (
+    <p className="text-3xl">
+      <GigsCard {...DUMMY} />
+    </p>
+  );
 }
 
 export default App;
--- frontend/src/components/GigsCard.tsx
+++ frontend/src/components/GigsCard.tsx
@@ -1,1 +1,41 @@
+import { FC } from "react";
 
+interface User {
+  name: string;
+  avatar: string;
+}
+interface GigsCardProps {
+  gigsTitle: string;
+  gigDescription: string;
+  gigPrice: string;
+  gigImage: string;
+  postedBy: User;
+}
+
+const GigsCard: FC<GigsCardProps> = ({
+  gigsTitle,
+  gigDescription,
+  gigPrice,
+  gigImage,
+  postedBy,
+}) => {
+  return (
+    <div className="flex flex-col items-start max-w-xs bg-white shadow-md p-4 rounded-lg gap-4 cursor-pointer">
+      <img className="w-64" src={gigImage} alt={gigImage} />
+      <div className="flex items-center gap-3">
+        <img className="w-10 rounded-full" src={postedBy.avatar}></img>
+        <p className="text-xl font-normal">{postedBy.name}</p>
+      </div>
+      <div className="flex flex-col items-start gap-1 my-3">
+        <p className="font-bold text-xl">{gigsTitle}</p>
+        <p className="text-left text-xl ">{gigDescription}</p>
+      </div>
+      <p className="font-bold text-lg">Rewards from {gigPrice}</p>
+      {/* <button className="p-3 text-lg bg-teal-500 border-2 border-gray-600 rounded-lg">
+        Apply
+      </button> */}
+    </div>
+  );
+};
+
+export default GigsCard;
